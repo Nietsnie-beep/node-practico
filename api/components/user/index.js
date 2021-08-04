f33c093fb0edba46fd0aaf213367f0c18dab00a3
unknown <AL02982380@tecmilenio.mx>
2021-08-04 16:41:53 -0500

    remotemysql reapair3

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,14 +1,15 @@
-
-//const store = require('../../../store/remote-mysql')
-const config = require('../../config')
+const store = require('../../../store/remote-mysql')
 const ctrl = require('./controller')
-
+const config = require("../../config");
+/*
 let store
 if (config.remoteDB === true){
     store = require('../../../store/remote-mysql');
 }else{
     store = require('../../../store/mysql')
 }
+
+*/
 
 
 module.exports = ctrl(store)
